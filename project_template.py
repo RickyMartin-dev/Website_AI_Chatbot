e215c124e93363a70cd9f4d8d9ec437ec01441c5
Ricky Martin <rickylenmartin@gmail.com>
2025-07-15 10:52:41 -0400

    Working Notebook version

---
+++ project_template.py
@@ -0,0 +1,35 @@
+# Imports
+import os
+from pathlib import Path
+import logging
+
+# Define Logging Format
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s')
+
+# specify files to create
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    "Notebooks",
+    f"src/__init__.py",
+    "requirements.txt",
+]
+
+# Go through list and create folders/files
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    # logic for directory
+    if filedir != "":
+        # create directory
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+    # Create the files
+    # check if file exists I do not want to replace
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+    else: # if file already exists
+        logging.info(f"{filename} already exists")
